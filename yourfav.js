36e6a1f5b2a74303ecbae90cedce20c38249d746
Sedtawut <sedtawut.sc@mail.kmutt.ac.th>
2021-02-10 12:48:13 +0700

    f

---
+++ yourfav.js
@@ -0,0 +1,4 @@
+var name = prompt('What your favorite character');
+var color = prompt('What your favorite color '+name);
+console.log(name+' is my favorie character');
+console.log(color+' is my favorie character');
